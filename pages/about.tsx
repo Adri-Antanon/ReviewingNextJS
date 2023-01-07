bb0d89aade45ad42928db5b2956f3a3b58df0b44
Adrian Antanon <adrianantanon@gmail.com>
2023-01-07 20:05:21 +0100

    FEAT: add new about page

---
+++ pages/about.tsx
@@ -0,0 +1,29 @@
+import styles from '../styles/Home.module.css';
+import { Inter } from '@next/font/google';
+import Link from 'next/link';
+import Head from 'next/head';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About - Adri</title>
+        <meta name="description" content="About Adri" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.center}>
+          <Link href="/" className={styles.card}>
+            <h2 className={inter.className}>
+              Home <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>Nuevo componente</p>
+          </Link>
+        </div>
+      </main>
+    </>
+  );
+}
